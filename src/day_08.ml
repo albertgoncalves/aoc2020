c7dda40511347974cd46ee441310f8ea59e8506a
albertgoncalves <goncalves.albert@gmail.com>
2020-12-13 15:50:05 -0500

    day 8, in the bank

---
+++ src/day_08.ml
@@ -0,0 +1,90 @@
+type instr = Nop | Acc | Jmp
+
+type halt = Normal | Loop
+
+let parse (s : string) : (instr * int) =
+    let x : instr = match String.sub s 0 3 with
+        | "nop" -> Nop
+        | "acc" -> Acc
+        | "jmp" -> Jmp
+        | _ ->
+            (
+                Printf.eprintf "parse instr\n";
+                exit 1
+            ) in
+    let n : int option =
+        String.sub s 5 ((String.length s) - 5) |> Prelude.str_to_int in
+    let n : int = match n with
+        | Some n -> n
+        | None ->
+            (
+                Printf.eprintf "parse int\n";
+                exit 1
+            ) in
+    if s.[4] = '+' then
+        (x, n)
+    else
+        (x, n * (-1))
+
+let run (xs : (instr * int) array) : (halt * int) =
+    let n : int = Array.length xs in
+    let memory : (int, int) Hashtbl.t = Hashtbl.create n in
+    let rec loop (x : int) (i : int) : (halt * int) =
+        if i = n then
+            (Normal, x)
+        else
+            match xs.(i) with
+                | (Nop, _) -> loop x (i + 1)
+                | (Acc, n) ->
+                    (
+                        match Hashtbl.find_opt memory i with
+                            | Some _ -> (Loop, x)
+                            | None ->
+                                (
+                                    let x : int = x + n in
+                                    Hashtbl.add memory i x;
+                                    loop x (i + 1)
+                                )
+                    )
+                | (Jmp, n) -> loop x (i + n) in
+    loop 0 0
+
+let brute_force (xs : (instr * int) array) : (int * int) =
+    let rec loop (i : int) : (int * int) =
+        match xs.(i) with
+            | (Nop, 0) | (Acc, _) -> loop (i + 1)
+            | (Jmp, n) ->
+                (
+                    xs.(i) <- (Nop, n);
+                    match run xs with
+                        | (Normal, n) -> (i, n)
+                        | (Loop, _) ->
+                            (
+                                xs.(i) <- (Jmp, n);
+                                loop (i + 1)
+                            )
+                )
+            | (Nop, n) ->
+                (
+                    xs.(i) <- (Jmp, n);
+                    match run xs with
+                        | (Normal, n) -> (i, n)
+                        | (Loop, _) ->
+                            (
+                                xs.(i) <- (Nop, n);
+                                loop (i + 1)
+                            )
+                ) in
+    loop 0
+
+let () : unit =
+    let xs : (instr * int) array =
+        Prelude.read_file Sys.argv.(1)
+        |> Prelude.split_newlines
+        |> Array.map parse in
+    (
+        match run xs with
+            | (Normal, n) -> Printf.printf "Normal n:%d\n" n
+            | (Loop, n) ->  Printf.printf "Loop n:%d\n" n
+    );
+    brute_force xs |> (fun (i, n) -> Printf.printf "i:%d n:%d\n" i n)
