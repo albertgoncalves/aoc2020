b739995a6c902b81f437d2455e0d888d27c1c761
albertgoncalves <goncalves.albert@gmail.com>
2020-12-21 08:44:41 -0500

    day 19, part 1 done

--- src/day_19.ml
+++ src/day_19.ml
@@ -3,6 +3,80 @@ type t =
     | Alt of (int array * int array)
     | Lit of char
 
+let parse_index (s : string) : (int * int) option =
+    if Prelude.is_digit s.[0] then
+        let rec loop (i : int) : (int * int) option =
+            if s.[i] = ':' then
+                let x : int =
+                    String.sub s 0 i |> Prelude.str_to_int |> Option.get in
+                Some (i, x)
+            else
+                loop (i + 1) in
+        loop 0
+    else
+        None
+
+let queue_to_array (xs : 'a Queue.t) : 'a array =
+    Queue.to_seq xs |> Array.of_seq
+
+let rec parse_rule (s : string) (i : int) : t =
+    if s.[i] = '"' then
+        Lit s.[i + 1]
+    else
+        let xs : int Queue.t = Queue.create () in
+        let n : int = String.length s in
+        let f (i : int) (j : int) : unit =
+            if i <> j then (
+                let x : int =
+                    String.sub s i (j - i)
+                    |> Prelude.str_to_int
+                    |> Option.get in
+                Queue.add x xs
+            ) in
+        let rec loop (i : int) (j : int) : t =
+            if j = n then (
+                f i j;
+                Seq (queue_to_array xs)
+            ) else
+                match s.[j] with
+                    | '|' ->
+                        (
+                            match parse_rule s (j + 1) with
+                                | Seq xs' ->
+                                    Alt (queue_to_array xs, xs')
+                                | Alt _ | Lit _ ->
+                                    (
+                                        Printf.eprintf "parse_rule\n";
+                                        exit 1
+                                    )
+                        )
+                    | ' ' ->
+                        (
+                            if i <> j then (
+                                f i j
+                            );
+                            let j : int = j + 1 in
+                            loop j j
+                        )
+                    | _ -> loop i (j + 1) in
+        loop i i
+
+let parse (xs : string array) : ((int, t) Hashtbl.t * string array) =
+    let n : int = Array.length xs in
+    let rules : (int, t) Hashtbl.t = Hashtbl.create n in
+    let rec loop (i : int) : int =
+        let s : string = xs.(i) in
+        match parse_index s with
+            | Some (j, index) ->
+                (
+                    let rule : t = parse_rule s (j + 2) in
+                    Hashtbl.add rules index rule;
+                    loop (i + 1)
+                )
+            | None -> i in
+    let i : int = loop 0 in
+    (rules, Array.sub xs i (n - i))
+
 let rec traverse
         (s : string)
         (i : int)
@@ -35,29 +109,16 @@ let rec traverse
                 else
                     None
 
+let tally (rules : (int, t) Hashtbl.t) (s : string) : int =
+    match traverse s 0 rules (Hashtbl.find rules 0) with
+        | Some i when i = String.length s -> 1
+        | _ -> 0
+
 let () : unit =
-    let rules : (int, t) Hashtbl.t = Hashtbl.create 6 in
-    Array.iter
-        (fun (i, r) -> Hashtbl.add rules i r)
-        [|
-            (4, Lit 'a');
-            (5, Lit 'b');
-            (1, Alt ([|2; 3|], [|3; 2|]));
-            (2, Alt ([|4; 4|], [|5; 5|]));
-            (3, Alt ([|4; 5|], [|5; 4|]));
-            (0, Seq [|4; 1; 5|]);
-        |];
-    let f (s : string) : unit =
-        let x : bool =
-            match traverse s 0 rules (Hashtbl.find rules 0) with
-                | Some i when i = String.length s -> true
-                | _ -> false in
-        Printf.printf "%-10S -> %b\n" s x in
-    let xs : string array = [|
-        "ababbb";
-        "bababa";
-        "abbbab";
-        "aaabbb";
-        "aaaabbb";
-    |] in
-    Array.iter f xs
+    let (rules, xs) : ((int, t) Hashtbl.t * string array) =
+        Prelude.read_file Sys.argv.(1)
+        |> Prelude.split_newlines
+        |> parse in
+    Array.map (tally rules) xs
+    |> Array.fold_left (+) 0
+    |> Printf.printf "%d\n"
