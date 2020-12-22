115e3e2e400015084cc20874586a52f39d26c443
Albert Goncalves <goncalves.albert@gmail.com>
2020-12-22 15:56:23 -0500

    clean-up

--- src/day_21.ml
+++ src/day_21.ml
@@ -12,7 +12,7 @@ let push (xs : string list) (s : string) (i : int) (j : int) : string list =
 let parse_parens (s : string) (i : int) : string list =
     let rec loop (xs : string list) (i : int) (j : int) : string list =
         match s.[j] with
-            | ')' -> push xs s i j |> List.filter ((<>) "contains")
+            | ')' -> push xs s i j
             | ',' | ' ' ->
                 let xs : string list = push xs s i j in
                 let i : int = j + 1 in
@@ -26,7 +26,8 @@ let parse (s : string) : (string list * string list) =
             (i : int)
             (j : int) : (string list * string list) =
         match s.[j] with
-            | '(' -> (parse_parens s (j + 1), xs)
+            | '(' ->
+                (parse_parens s (j + 1) |> List.filter ((<>) "contains"), xs)
             | ' ' ->
                 let xs : string list = push xs s i j in
                 let i : int = j + 1 in
@@ -46,7 +47,7 @@ let insert
     List.iter f keys;
     value
 
-let reduce (table : (string, StrSet.t) Hashtbl.t) (key : string) : unit =
+let shrink (table : (string, StrSet.t) Hashtbl.t) (key : string) : unit =
     let value : StrSet.t = Hashtbl.find table key in
     if StrSet.cardinal value <> 1 then
         ()
@@ -74,7 +75,7 @@ let continue (table : (string, StrSet.t) Hashtbl.t) : bool =
             else
                 true in
     let keys : string list = Hashtbl.to_seq_keys table |> List.of_seq in
-    List.iter (reduce table) keys;
+    List.iter (shrink table) keys;
     loop keys
 
 let show (key : string) (value : StrSet.t) : unit =
@@ -103,16 +104,15 @@ let () : unit =
         let union : StrSet.t =
             Hashtbl.to_seq_values table
             |> Seq.fold_left (fun a b -> StrSet.union a b) StrSet.empty in
-        let xs : string list =
-            List.filter (fun x -> StrSet.mem x union |> not) xs in
-        List.length xs |> Printf.printf "%d\n"
+        List.filter (fun x -> StrSet.mem x union |> not) xs
+        |> List.length
+        |> Printf.printf "%d\n"
     );
     (
-        let keys_values : (string * string) list =
-            Hashtbl.to_seq table
-            |> List.of_seq
-            |> List.map (fun (k, v) -> (k, StrSet.choose v))
-            |> List.sort (fun (a, _) (b, _) -> compare a b) in
-        List.iter (fun (_, value) -> Printf.printf "%s," value) keys_values;
+        Hashtbl.to_seq table
+        |> List.of_seq
+        |> List.map (fun (k, v) -> (k, StrSet.choose v))
+        |> List.sort (fun (a, _) (b, _) -> compare a b)
+        |> List.iter (fun (_, value) -> Printf.printf "%s," value);
         Printf.printf "\n"
     )
